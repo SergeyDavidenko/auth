8bd17c90c202df9d9260e15e0733d8e056069975
Sergey Davidenko <sergey.davidenko@ardas.biz>
2020-03-31 11:34:11 +0300

    first commit

---
+++ client/client.go
@@ -0,0 +1,42 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// AuthCheck ...
+func AuthCheck(url, token string) (bool error) {
+	if token == "" {
+		return false, fmt.Errorf("token not set")
+	}
+	if url == "" {
+		return false, fmt.Errorf("auth url not set")
+	}
+	var tokenJSON tokenValidator
+	tokenJSON.token = token
+	jsonStr, errMarshal := json.Marshal(tokenJSON)
+	if errMarshal != nil {
+		return false, fmt.Errorf("cannot json marshal struct")
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	if resp.Status != 200 {
+		return false, fmt.Errorf("status conde not qe 200")
+	}
+	return true, nil
+}
+
+type tokenValidator struct {
+	token string `json:"token"`
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SergeyDavidenko/auth
+
+go 1.13
